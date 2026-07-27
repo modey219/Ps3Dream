2dd9c63e5d6394165216353911c8c03c99e91497
modey219 <shajor149@gmail.com>
2026-07-27 13:36:30 +0300

    fix: rewrite jni.h stub to use function table pattern

--- aps3e-extracted/aps3e-2.40/app/src/main/cpp/ffmpeg_stubs/jni.h
+++ aps3e-extracted/aps3e-2.40/app/src/main/cpp/ffmpeg_stubs/jni.h
@@ -1,5 +1,5 @@
 #pragma once
-// Stub jni.h for iOS build (Android JNI types not used at runtime)
+// Minimal JNI stub for iOS build
 
 #include <stdint.h>
 #include <stddef.h>
@@ -8,6 +8,7 @@
 #define JNICALL
 #define JNI_FALSE 0
 #define JNI_TRUE 1
+#define JNI_VERSION_1_6 0x00010006
 
 typedef int32_t jint;
 typedef int64_t jlong;
@@ -26,51 +27,33 @@ typedef jobject jintArray;
 typedef jobject jbyteArray;
 typedef jobject jfloatArray;
 typedef jobject jdoubleArray;
+typedef void* jfieldID;
+typedef void* jmethodID;
 
-struct _JNIEnv;
-struct _JavaVM;
+struct JNIEnv_;
+struct JavaVM_;
+typedef JNIEnv_ JNIEnv;
+typedef JavaVM_ JavaVM;
 
-typedef _JNIEnv JNIEnv;
-typedef _JavaVM JavaVM;
-
-struct JNINativeInterface;
-
-struct _JNIEnv {
+struct JNINativeInterface_ {
+    void* reserved0; void* reserved1; void* reserved2; void* reserved3;
     jclass (*FindClass)(JNIEnv*, const char*);
-    jobject (*NewObject)(JNIEnv*, jclass, ...);
-    jobject (*NewObjectV)(JNIEnv*, jclass, void*);
+    jmethodID (*GetMethodID)(JNIEnv*, jclass, const char*, const char*);
+    jmethodID (*GetStaticMethodID)(JNIEnv*, jclass, const char*, const char*);
+    jfieldID (*GetFieldID)(JNIEnv*, jclass, const char*, const char*);
+    jfieldID (*GetStaticFieldID)(JNIEnv*, jclass, const char*, const char*);
+    jobject (*CallObjectMethodV)(JNIEnv*, jobject, jmethodID, void*);
+    jint (*CallIntMethodV)(JNIEnv*, jobject, jmethodID, void*);
+    jlong (*CallLongMethodV)(JNIEnv*, jobject, jmethodID, void*);
+    jboolean (*CallBooleanMethodV)(JNIEnv*, jobject, jmethodID, void*);
+    void (*CallVoidMethodV)(JNIEnv*, jobject, jmethodID, void*);
+    jobject (*CallStaticObjectMethodV)(JNIEnv*, jclass, jmethodID, void*);
+    jint (*CallStaticIntMethodV)(JNIEnv*, jclass, jmethodID, void*);
+    void (*CallStaticVoidMethodV)(JNIEnv*, jclass, jmethodID, void*);
     jstring (*NewStringUTF)(JNIEnv*, const char*);
     const char* (*GetStringUTFChars)(JNIEnv*, jstring, jboolean*);
     void (*ReleaseStringUTFChars)(JNIEnv*, jstring, const char*);
-    jint (*GetIntField)(JNIEnv*, jobject, int);
-    jlong (*GetLongField)(JNIEnv*, jobject, int);
-    void (*SetIntField)(JNIEnv*, jobject, int, jint);
-    void (*SetLongField)(JNIEnv*, jobject, int, jlong);
-    void (*SetBooleanField)(JNIEnv*, jobject, int, jboolean);
-    jboolean (*GetBooleanField)(JNIEnv*, jobject, int);
-    jbyte (*GetByteField)(JNIEnv*, jobject, int);
-    jfloat (*GetFloatField)(JNIEnv*, jobject, int);
-    jdouble (*GetDoubleField)(JNIEnv*, jobject, int);
-    jobject (*GetObjectField)(JNIEnv*, jobject, int);
-    void (*SetObjectField)(JNIEnv*, jobject, int, jobject);
-    jobject (*CallObjectMethod)(JNIEnv*, jobject, int, ...);
-    jobject (*CallObjectMethodV)(JNIEnv*, jobject, int, void*);
-    jint (*CallIntMethod)(JNIEnv*, jobject, int, ...);
-    jint (*CallIntMethodV)(JNIEnv*, jobject, int, void*);
-    jlong (*CallLongMethod)(JNIEnv*, jobject, int, ...);
-    jboolean (*CallBooleanMethod)(JNIEnv*, jobject, int, ...);
-    void (*CallVoidMethod)(JNIEnv*, jobject, int, ...);
-    void (*CallVoidMethodV)(JNIEnv*, jobject, int, void*);
-    jobject (*CallStaticObjectMethod)(JNIEnv*, jclass, int, ...);
-    jint (*CallStaticIntMethod)(JNIEnv*, jclass, int, ...);
-    void (*CallStaticVoidMethod)(JNIEnv*, jclass, int, ...);
-    jboolean (*ExceptionCheck)(JNIEnv*);
-    void (*ExceptionClear)(JNIEnv*);
-    void (*ExceptionDescribe)(JNIEnv*);
-    jobject (*NewGlobalRef)(JNIEnv*, jobject);
-    void (*DeleteGlobalRef)(JNIEnv*, jobject);
-    jobject (*NewLocalRef)(JNIEnv*, jobject);
-    void (*DeleteLocalRef)(JNIEnv*, jobject);
+    jobject (*NewObjectV)(JNIEnv*, jclass, jmethodID, void*);
     jint (*GetArrayLength)(JNIEnv*, jarray);
     jobject (*GetObjectArrayElement)(JNIEnv*, jobjectArray, jint);
     void (*SetObjectArrayElement)(JNIEnv*, jobjectArray, jint, jobject);
@@ -82,60 +65,90 @@ struct _JNIEnv {
     void (*SetByteArrayRegion)(JNIEnv*, jbyteArray, jint, jint, const jbyte*);
     void (*GetIntArrayRegion)(JNIEnv*, jintArray, jint, jint, jint*);
     void (*SetIntArrayRegion)(JNIEnv*, jintArray, jint, jint, const jint*);
-    jfieldID (*GetFieldID)(JNIEnv*, jclass, const char*, const char*);
-    jfieldID (*GetStaticFieldID)(JNIEnv*, jclass, const char*, const char*);
-    jmethodID (*GetMethodID)(JNIEnv*, jclass, const char*, const char*);
-    jmethodID (*GetStaticMethodID)(JNIEnv*, jclass, const char*, const char*);
+    jboolean (*ExceptionCheck)(JNIEnv*);
+    void (*ExceptionClear)(JNIEnv*);
+    void (*ExceptionDescribe)(JNIEnv*);
+    jobject (*NewGlobalRef)(JNIEnv*, jobject);
+    void (*DeleteGlobalRef)(JNIEnv*, jobject);
+    jobject (*NewLocalRef)(JNIEnv*, jobject);
+    void (*DeleteLocalRef)(JNIEnv*, jobject);
+    jclass (*GetObjectClass)(JNIEnv*, jobject);
     jclass (*DefineClass)(JNIEnv*, const char*, jobject, const jbyte*, jsize);
-
-    // Inline method dispatchers
-    jclass FindClass(const char* name) { return FindClass(this, name); }
-    jstring NewStringUTF(const char* str) { return NewStringUTF(this, str); }
-    const char* GetStringUTFChars(jstring s, jboolean* b) { return GetStringUTFChars(this, s, b); }
-    void ReleaseStringUTFChars(jstring s, const char* c) { ReleaseStringUTFChars(this, s, c); }
-    jint GetIntField(jobject o, int f) { return GetIntField(this, o, f); }
-    jlong GetLongField(jobject o, int f) { return GetLongField(this, o, f); }
-    void SetIntField(jobject o, int f, jint v) { SetIntField(this, o, f, v); }
-    void SetLongField(jobject o, int f, jlong v) { SetLongField(this, o, f, v); }
-    void SetBooleanField(jobject o, int f, jboolean v) { SetBooleanField(this, o, f, v); }
-    jboolean GetBooleanField(jobject o, int f) { return GetBooleanField(this, o, f); }
-    jbyte GetByteField(jobject o, int f) { return GetByteField(this, o, f); }
-    jfloat GetFloatField(jobject o, int f) { return GetFloatField(this, o, f); }
-    jdouble GetDoubleField(jobject o, int f) { return GetDoubleField(this, o, f); }
-    jobject GetObjectField(jobject o, int f) { return GetObjectField(this, o, f); }
-    void SetObjectField(jobject o, int f, jobject v) { SetObjectField(this, o, f, v); }
-    jobject NewObject(jclass cls, ...) { return nullptr; }
-    jboolean ExceptionCheck() { return ExceptionCheck(this); }
-    void ExceptionClear() { ExceptionClear(this); }
-    void ExceptionDescribe() { ExceptionDescribe(this); }
-    jobject NewGlobalRef(jobject o) { return o; }
-    void DeleteGlobalRef(jobject o) { (void)o; }
-    jobject NewLocalRef(jobject o) { return o; }
-    void DeleteLocalRef(jobject o) { (void)o; }
-    jint GetArrayLength(jarray a) { return GetArrayLength(this, a); }
-    jobject GetObjectArrayElement(jobjectArray a, jint i) { return GetObjectArrayElement(this, a, i); }
-    void SetObjectArrayElement(jobjectArray a, jint i, jobject o) { SetObjectArrayElement(this, a, i, o); }
-    jfieldID GetFieldID(jclass c, const char* n, const char* s) { return GetFieldID(this, c, n, s); }
-    jfieldID GetStaticFieldID(jclass c, const char* n, const char* s) { return GetStaticFieldID(this, c, n, s); }
-    jmethodID GetMethodID(jclass c, const char* n, const char* s) { return GetMethodID(this, c, n, s); }
-    jmethodID GetStaticMethodID(jclass c, const char* n, const char* s) { return GetStaticMethodID(this, c, n, s); }
-    jclass GetObjectClass(jobject o) { return nullptr; }
+    jobject (*NewObjectArray)(JNIEnv*, jint, jclass, jobject);
+    jstring (*NewString)(JNIEnv*, const jchar*, jsize);
+    void (*SetBooleanField)(JNIEnv*, jobject, jfieldID, jboolean);
+    void (*SetIntField)(JNIEnv*, jobject, jfieldID, jint);
+    void (*SetLongField)(JNIEnv*, jobject, jfieldID, jlong);
+    void (*SetObjectField)(JNIEnv*, jobject, jfieldID, jobject);
+    jboolean (*GetBooleanField)(JNIEnv*, jobject, jfieldID);
+    jint (*GetIntField)(JNIEnv*, jobject, jfieldID);
+    jlong (*GetLongField)(JNIEnv*, jobject, jfieldID);
+    jobject (*GetObjectField)(JNIEnv*, jobject, jfieldID);
+    jfloat (*GetFloatField)(JNIEnv*, jobject, jfieldID);
+    jdouble (*GetDoubleField)(JNIEnv*, jobject, jfieldID);
+    jbyte (*GetByteField)(JNIEnv*, jobject, jfieldID);
 };
 
-typedef jint (*JNI_OnLoad_func)(JavaVM*, void*);
+struct JNIEnv_ {
+    const JNINativeInterface_* functions;
+    jclass FindClass(const char* n) { return functions->FindClass(this, n); }
+    jmethodID GetMethodID(jclass c, const char* n, const char* s) { return functions->GetMethodID(this, c, n, s); }
+    jmethodID GetStaticMethodID(jclass c, const char* n, const char* s) { return functions->GetStaticMethodID(this, c, n, s); }
+    jfieldID GetFieldID(jclass c, const char* n, const char* s) { return functions->GetFieldID(this, c, n, s); }
+    jfieldID GetStaticFieldID(jclass c, const char* n, const char* s) { return functions->GetStaticFieldID(this, c, n, s); }
+    jstring NewStringUTF(const char* s) { return functions->NewStringUTF(this, s); }
+    const char* GetStringUTFChars(jstring s, jboolean* b) { return functions->GetStringUTFChars(this, s, b); }
+    void ReleaseStringUTFChars(jstring s, const char* c) { functions->ReleaseStringUTFChars(this, s, c); }
+    jint GetArrayLength(jarray a) { return functions->GetArrayLength(this, a); }
+    jobject GetObjectArrayElement(jobjectArray a, jint i) { return functions->GetObjectArrayElement(this, a, i); }
+    void SetObjectArrayElement(jobjectArray a, jint i, jobject o) { functions->SetObjectArrayElement(this, a, i, o); }
+    jobject NewGlobalRef(jobject o) { return functions->NewGlobalRef(this, o); }
+    void DeleteGlobalRef(jobject o) { functions->DeleteGlobalRef(this, o); }
+    jobject NewLocalRef(jobject o) { return functions->NewLocalRef(this, o); }
+    void DeleteLocalRef(jobject o) { functions->DeleteLocalRef(this, o); }
+    jclass GetObjectClass(jobject o) { return functions->GetObjectClass(this, o); }
+    jboolean ExceptionCheck() { return functions->ExceptionCheck(this); }
+    void ExceptionClear() { functions->ExceptionClear(this); }
+    void ExceptionDescribe() { functions->ExceptionDescribe(this); }
+    void SetBooleanField(jobject o, jfieldID f, jboolean v) { functions->SetBooleanField(this, o, f, v); }
+    void SetIntField(jobject o, jfieldID f, jint v) { functions->SetIntField(this, o, f, v); }
+    void SetLongField(jobject o, jfieldID f, jlong v) { functions->SetLongField(this, o, f, v); }
+    void SetObjectField(jobject o, jfieldID f, jobject v) { functions->SetObjectField(this, o, f, v); }
+    jboolean GetBooleanField(jobject o, jfieldID f) { return functions->GetBooleanField(this, o, f); }
+    jint GetIntField(jobject o, jfieldID f) { return functions->GetIntField(this, o, f); }
+    jlong GetLongField(jobject o, jfieldID f) { return functions->GetLongField(this, o, f); }
+    jobject GetObjectField(jobject o, jfieldID f) { return functions->GetObjectField(this, o, f); }
+    jfloat GetFloatField(jobject o, jfieldID f) { return functions->GetFloatField(this, o, f); }
+    jdouble GetDoubleField(jobject o, jfieldID f) { return functions->GetDoubleField(this, o, f); }
+    jbyte GetByteField(jobject o, jfieldID f) { return functions->GetByteField(this, o, f); }
+    jobject NewObject(jclass c, jmethodID m, ...) { return nullptr; }
+    jint* GetIntArrayElements(jintArray a, jboolean* b) { return functions->GetIntArrayElements(this, a, b); }
+    void ReleaseIntArrayElements(jintArray a, jint* e, jint m) { functions->ReleaseIntArrayElements(this, a, e, m); }
+    jbyte* GetByteArrayElements(jbyteArray a, jboolean* b) { return functions->GetByteArrayElements(this, a, b); }
+    void ReleaseByteArrayElements(jbyteArray a, jbyte* e, jint m) { functions->ReleaseByteArrayElements(this, a, e, m); }
+    void GetByteArrayRegion(jbyteArray a, jint s, jint l, jbyte* b) { functions->GetByteArrayRegion(this, a, s, l, b); }
+    void SetByteArrayRegion(jbyteArray a, jint s, jint l, const jbyte* b) { functions->SetByteArrayRegion(this, a, s, l, b); }
+    void GetIntArrayRegion(jintArray a, jint s, jint l, jint* b) { functions->GetIntArrayRegion(this, a, s, l, b); }
+    void SetIntArrayRegion(jintArray a, jint s, jint l, const jint* b) { functions->SetIntArrayRegion(this, a, s, l, b); }
+    jobject NewObjectArray(jint l, jclass c, jobject i) { return functions->NewObjectArray(this, l, c, i); }
+    jclass DefineClass(const char* n, jobject l, const jbyte* b, jsize s) { return functions->DefineClass(this, n, l, b, s); }
+    jobject CallObjectMethod(jobject o, jmethodID m, ...) { return nullptr; }
+    jint CallIntMethod(jobject o, jmethodID m, ...) { return 0; }
+    jlong CallLongMethod(jobject o, jmethodID m, ...) { return 0; }
+    jboolean CallBooleanMethod(jobject o, jmethodID m, ...) { return 0; }
+    void CallVoidMethod(jobject o, jmethodID m, ...) {}
+    jobject CallStaticObjectMethod(jclass c, jmethodID m, ...) { return nullptr; }
+    jint CallStaticIntMethod(jclass c, jmethodID m, ...) { return 0; }
+    void CallStaticVoidMethod(jclass c, jmethodID m, ...) {}
+    void SetStaticIntField(jclass c, jfieldID f, jint v) {}
+};
 
-struct _JavaVM {
+struct JavaVM_ {
     jint (*GetEnv)(JavaVM*, void**, jint);
     jint (*AttachCurrentThread)(JavaVM*, JNIEnv**, void*);
     jint (*DetachCurrentThread)(JavaVM*);
-    jint (*GetDefaultJavaVMInitArgs)(void*);
-
     jint GetEnv(void** env, jint ver) { return GetEnv(this, env, ver); }
     jint AttachCurrentThread(JNIEnv** env, void* a) { return AttachCurrentThread(this, env, a); }
 };
 
-// JNI field/method IDs
-typedef void* jfieldID;
-typedef void* jmethodID;
-
-#define JNI_VERSION_1_6 0x00010006
+typedef jint (*JNI_OnLoad_func)(JavaVM*, void*);
